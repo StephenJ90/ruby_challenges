87e9f29f28ca71bd4c2a820294207367937d8c93
Stephen Johnston <sdjohnston1990@gmail.com>
2020-05-30 15:41:41 -0400

    Add files for easy_1 in ruby challenges

--- easy_1/series.rb
+++ easy_1/series.rb
@@ -1,12 +1,10 @@
 class Series
-  attr_reader :series
-
   def initialize(series)
     @series = series
   end
 
   def slices(n)
-    raise ArgumentError if n > series.size
-    series.each_char.map(&:to_i).each_cons(n).to_a
+    raise ArgumentError if n > @series.size
+    @series.each_char.map(&:to_i).each_cons(n).to_a
   end
 end
